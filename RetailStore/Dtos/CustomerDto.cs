d07c61de8aef454266e6f2601ff2a262a48f15f2
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-06-15 11:18:20 +0530

    Added DTOs for Product and Customer

---
+++ RetailStore/Dtos/CustomerDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RetailStore.Dtos;
+
+public class CustomerDto
+{
+    /// <summary>
+    /// Gets and sets the Customer Name
+    /// </summary>
+    public string CustomerName { get; set; }
+
+    /// <summary>
+    /// Gets and sets the Phone Number of the Customer
+    /// </summary>
+    public long PhoneNumber { get; set; }
+}
---
+++ RetailStore/Dtos/ProductDto.cs
@@ -0,0 +1,14 @@
+﻿namespace RetailStore.Dtos;
+
+public class ProductDto
+{
+    /// <summary>
+    /// Gets and sets Product Name
+    /// </summary>
+    public string ProductName { get; set; }
+
+    /// <summary>
+    /// Gets and sets Product Price
+    /// </summary>
+    public decimal ProductPrice { get; set; }
+}
