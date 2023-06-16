d56a157d3adfc268524e58f34f36954497cdd1e7
josmy.abraham99 <josmykabraham2306@gmail.com>
2023-06-16 12:27:29 +0530

    collection per day

--- RetailStore/Dtos/OrderDetailDto.cs
+++ RetailStore/Dtos/OrderDetailDto.cs
@@ -5,6 +5,16 @@
 /// </summary>
 public class OrderDetailDto
 {
+
+    /// <summary>
+    /// Gets and sets Unique name of product entity
+    /// </summary>
+    /// <example> "Soap" </example>
     public string ProductName { get; set; }
+
+    /// <summary>
+    /// Gets and sets the quantity of product ordered .
+    /// </summary>
+    /// <example> 10 </example>
     public int Quantity { get; set; }
 }
