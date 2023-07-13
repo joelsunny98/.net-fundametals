1da6776d67d97c1666ff1f1ba7adc8504be72be6
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-07-13 12:15:06 +0530

    Added Create Order Command Validator

---
+++ RetailStore/Features/OrderManagement/Commands/CreateOrderCommandValidator.cs
@@ -0,0 +1,17 @@
+﻿using FluentValidation;
+using RetailStore.Dtos;
+
+namespace RetailStore.Features.OrderManagement.Commands;
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Data.CustomerId).NotNull().NotEmpty();
+        RuleForEach(x => x.Data.Details).ChildRules(p =>
+        {
+            p.RuleFor(x => x.ProductId).NotNull().NotEmpty();
+            p.RuleFor(x => x.Quantity).NotNull().NotEmpty();
+        });
+    }
+}
