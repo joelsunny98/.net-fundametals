818c662e684e4090fb466287e08c227360319bc2
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-07-13 13:28:41 +0530

    Added Update Order Command and Validation

--- RetailStore/Features/OrderManagement/Commands/UpdateOrderByIdCommandValidator.cs
+++ RetailStore/Features/OrderManagement/Commands/UpdateOrderByIdCommandValidator.cs
@@ -1,22 +1,21 @@
 ﻿using FluentValidation;
 using RetailStore.Constants;
-using RetailStore.Dtos;
 
 namespace RetailStore.Features.OrderManagement.Commands;
 
 /// <summary>
-/// Validator for Create Order Command
+/// Validator for Update Order Command
 /// </summary>
-public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+public class UpdateOrderByIdCommandValidator: AbstractValidator<UpdateOrderByIdCommand>
 {
     /// <summary>
     /// Validator for defining specific rules for properties
     /// </summary>
-    public CreateOrderCommandValidator()
+    public UpdateOrderByIdCommandValidator()
     {
         //Rules for required fields
-        RuleFor(x => x.Data.CustomerId).NotNull().NotEmpty().WithMessage(ValidationMessage.Required);
-        RuleForEach(x => x.Data.Details).ChildRules(p =>
+        RuleFor(x => x.OrderRequest.CustomerId).NotNull().NotEmpty().WithMessage(ValidationMessage.Required);
+        RuleForEach(x => x.OrderRequest.Details).ChildRules(p =>
         {
             p.RuleFor(x => x.ProductId).NotNull().NotEmpty().WithMessage(ValidationMessage.Required);
             p.RuleFor(x => x.Quantity).NotNull().NotEmpty().WithMessage(ValidationMessage.Required);
