ebe06cac112f4d64ffd5f750f6d079ccf88c127a
josmy.abraham99 <josmykabraham2306@gmail.com>
2023-07-17 12:23:35 +0530

    logger in customerbyid

--- RetailStore/Requests/CustomerManagement/GetCustomersQuery.cs
+++ RetailStore/Requests/CustomerManagement/GetCustomersQuery.cs
@@ -12,20 +12,36 @@ public class GetCustomersQuery : IRequest<List<CustomerDto>>
 public class GetCustomerQueryHandler : IRequestHandler<GetCustomersQuery, List<CustomerDto>>
 {
     private readonly RetailStoreDbContext _dbContext;
+    private readonly ILogger<GetCustomerQueryHandler> _logger;
 
-    public GetCustomerQueryHandler(RetailStoreDbContext dbContext)
+
+    public GetCustomerQueryHandler(RetailStoreDbContext dbContext, ILogger<GetCustomerQueryHandler> logger)
     {
         _dbContext = dbContext;
+        _logger = logger;
     }
 
     public async Task<List<CustomerDto>> Handle(GetCustomersQuery request, CancellationToken cancellationToken)
     {
-        var result = await _dbContext.Customers.Select(x => new CustomerDto
+        try
         {
-            CustomerName = x.Name,
-            PhoneNumber = (long)x.PhoneNumber
-        }).ToListAsync();
+            var result = await _dbContext.Customers
+                .Select(x => new CustomerDto
+                {
+                    CustomerName = x.Name,
+                    PhoneNumber = (long)x.PhoneNumber
+                })
+                .ToListAsync();
 
-        return result;
+            _logger.LogInformation("Retrieved {CustomerCount} customers", result.Count);
+
+            return result;
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, "Failed to retrieve customers");
+            throw;
+        }
     }
+
 }
