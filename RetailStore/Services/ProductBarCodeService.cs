3098117e22fb4e25f24a9a50aadd7ffc9d88f158
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-07-18 10:44:54 +0530

    Added Logger to Barcode Service

--- RetailStore/Services/ProductBarCodeService.cs
+++ RetailStore/Services/ProductBarCodeService.cs
@@ -9,10 +9,12 @@ namespace RetailStore.Services;
 public class ProductBarCodeService : IProductBarCodeService
 {
     private readonly RetailStoreDbContext _dbContext;
+    private readonly ILogger _logger;
   
-    public ProductBarCodeService(RetailStoreDbContext dbContext)
+    public ProductBarCodeService(RetailStoreDbContext dbContext, ILogger<ProductBarCodeService> logger)
     {
         _dbContext = dbContext;
+        _logger = logger;
     } 
 
     public async Task<GeneratedBarcode> GeneratedBarcode(int id)
@@ -21,16 +23,13 @@ public class ProductBarCodeService : IProductBarCodeService
 
         if (product == null)
         {
+            _logger.LogError("Prouct with {ProductId} not found", product.Id);
             throw new KeyNotFoundException(nameof(product));
         }
 
         var barcode = BarcodeWriter.CreateBarcode(product.Name.ToString(), BarcodeWriterEncoding.Code128);
 
-        
-
-        Image barcodeImage = barcode.Image;
-
-
+        _logger.LogInformation("Barcode for Product {ProductId} generated", product.Id);
         return barcode;
     }
 }
