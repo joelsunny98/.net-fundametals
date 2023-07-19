293bca95d2425266c758521a572b9953b7310de9
josmy.abraham99 <josmykabraham2306@gmail.com>
2023-07-19 12:06:27 +0530

    uniquephonenumber

--- RetailStore/Requests/CustomerManagement/AddCustomerCommandValidator.cs
+++ RetailStore/Requests/CustomerManagement/AddCustomerCommandValidator.cs
@@ -1,32 +1,43 @@
 ﻿using FluentValidation;
-using Microsoft.EntityFrameworkCore;
 using RetailStore.Constants;
+using RetailStore.Persistence;
 
-namespace RetailStore.Requests.CustomerManagement;
-
-/// <summary>
-/// Validator for Add Customer Command
-/// </summary>
-public class AddCustomerCommandValidator : AbstractValidator<AddCustomerCommand>
+namespace RetailStore.Requests.CustomerManagement
 {
     /// <summary>
-    /// Validator for defining specific rules for properties
+    /// Validator for Add Customer Command
     /// </summary>
-    public AddCustomerCommandValidator()
+    public class AddCustomerCommandValidator : AbstractValidator<AddCustomerCommand>
     {
-        RuleFor(command => command.CustomerName)
-            .NotNull().NotEmpty().WithMessage(ValidationMessage.Required)
-            .MaximumLength(25).WithMessage(ValidationMessage.Length);
+        private readonly RetailStoreDbContext _dbContext;
 
-        RuleFor(command => command.PhoneNumber)
+        /// <summary>
+        /// Validator for defining specific rules for properties
+        /// </summary>
+        public AddCustomerCommandValidator(RetailStoreDbContext dbContext)
+        {
+            _dbContext = dbContext;
+
+            RuleFor(command => command.CustomerName)
                 .NotNull().NotEmpty().WithMessage(ValidationMessage.Required)
-                .Must(BeValidPhoneNumber).WithMessage(ValidationMessage.PhoneNumberLength);           
-    }
+                .MaximumLength(25).WithMessage(ValidationMessage.Length);
 
-    private bool BeValidPhoneNumber(long phoneNumber)
-    {
-        var phoneNumberString = phoneNumber.ToString();
-        return phoneNumberString.Length == 10;
-    }
+            RuleFor(command => command.PhoneNumber)
+                .NotNull().NotEmpty().WithMessage(ValidationMessage.Required)
+                .Must(BeValidPhoneNumber).WithMessage(ValidationMessage.PhoneNumberLength)
+                .Must(BeUniquePhoneNumber).WithMessage(ValidationMessage.UniquePhoneNumber);
+        }
 
+        private bool BeValidPhoneNumber(long phoneNumber)
+        {
+            var phoneNumberString = phoneNumber.ToString();
+            return phoneNumberString.Length == 10;
+        }
+
+        private bool BeUniquePhoneNumber(long phoneNumber)
+        {
+            var exists = _dbContext.Customers.Any(c => c.PhoneNumber == phoneNumber);
+            return !exists;
+        }
+    }
 }
