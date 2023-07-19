35bed5e3947305df58474352725130cea741bcbd
joel.sunny <joel.sunny@sreegcloudgmail.onmicrosoft.com>
2023-07-19 10:49:52 +0530

    Added Validation messages:

--- RetailStore/Requests/ProductManagement/AddProductCommandValidator.cs
+++ RetailStore/Requests/ProductManagement/AddProductCommandValidator.cs
@@ -1,5 +1,7 @@
 ﻿using FluentValidation;
+using Microsoft.EntityFrameworkCore;
 using RetailStore.Constants;
+using RetailStore.Persistence;
 
 namespace RetailStore.Requests.ProductManagement;
 
@@ -8,15 +10,30 @@ namespace RetailStore.Requests.ProductManagement;
 /// </summary>
 public class AddProductCommandValidator : AbstractValidator<AddProductCommand>
 {
+    private readonly RetailStoreDbContext _dbContext;
+
     /// <summary>
     /// Validation rules for specific properties
     /// </summary>
-    public AddProductCommandValidator()
+    public AddProductCommandValidator(RetailStoreDbContext dbcontext)
     {
+        _dbContext = dbcontext;
+
         RuleFor(command => command.ProductName).NotNull().NotEmpty()
-            .WithMessage(ValidationMessage.Required);
+            .WithMessage(ValidationMessage.Required)
+            .MaximumLength(50).WithMessage(command => string.Format(ValidationMessage.CharExceedsFifty, command.ProductName));
+
+        RuleFor(command => command.ProductName).MustAsync(IsUniqueProduct);
 
         RuleFor(command => command.ProductPrice).NotNull().NotEmpty()
-            .WithMessage(ValidationMessage.Required);
+            .WithMessage(ValidationMessage.Required)
+            .GreaterThan(0).WithMessage(ValidationMessage.GreaterThanZero);
+    }
+
+    private async Task<bool> IsUniqueProduct(string productName, CancellationToken cancellationToken) 
+    {
+        var product = await _dbContext.Products.FirstOrDefaultAsync(e => e.Name == productName);
+
+        return product != null;
     }
 }
