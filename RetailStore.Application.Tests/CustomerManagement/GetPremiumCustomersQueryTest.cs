719ca6957e5f82649de9955da69a9f9a9de4de5c
josmy.abraham99 <josmykabraham2306@gmail.com>
2023-08-11 12:40:30 +0530

    recommended

--- RetailStore.Application.Tests/CustomerManagement/GetPremiumCustomersQueryTest.cs
+++ RetailStore.Application.Tests/CustomerManagement/GetPremiumCustomersQueryTest.cs
@@ -1,51 +1,51 @@
-﻿//using Microsoft.Extensions.Logging;
-//using MockQueryable.Moq;
-//using Moq;
-//using RetailStore.Contracts;
-//using RetailStore.Model;
+﻿using Microsoft.Extensions.Logging;
+using MockQueryable.Moq;
+using Moq;
+using RetailStore.Contracts;
+using RetailStore.Model;
 
-//namespace RetailStore.Requests.CustomerManagement;
+namespace RetailStore.Requests.CustomerManagement;
 
-//public class GetPremiumCustomersQueryTest
-//{
-//    private readonly Mock<IRetailStoreDbContext> _mockDbContext;
-//    private readonly Mock<ILogger<GetCustomerQueryHandler>> _logger;
-//    public GetPremiumCustomersQueryTest()
-//    {
-//        _mockDbContext = new Mock<IRetailStoreDbContext>();
-//        _logger = new Mock<ILogger<GetPremiumCustomersQuery>>();
-//        _mockpremiumCodeService = new Mock<IPremiumCodeService>();
-//        MockPremiumOrderdata();
-//    }
-//    [Fact]
-//    public async Task Handle_ReturnsPremiumCustomers()
-//    {
-
-
+public class GetPremiumCustomersQueryTest
+{
+    private readonly Mock<IRetailStoreDbContext> _mockDbContext;
+    private readonly Mock<ILogger<GetCustomerQueryHandler>> _logger;
+    public GetPremiumCustomersQueryTest()
+    {
+        _mockDbContext = new Mock<IRetailStoreDbContext>();
+        _logger = new Mock<ILogger<GetPremiumCustomersQuery>>();
+        _mockpremiumCodeService = new Mock<IPremiumCodeService>();
+        MockPremiumOrderdata();
+    }
+    [Fact]
+    public async Task Handle_ReturnsPremiumCustomers()
+    {
 
 
-//    }
-
-//    #region DatabaseInitilization
-//    /// <summary>
-//    /// Initializes Mock database with mocked object
-//    /// </summary>
-//    private void MockPremiumOrderdata()
-//    {
-//        _mockDbContext.Setup(x => x.Orders).Returns(new List<Order>{new Order()
-//        {
-//             Id = 1,
-//             CustomerId = 2,
-//             TotalAmount = 2000,
-//             Customer = new Customer
-//             {
-//                 Name ="Vismaya",
-//                 PhoneNumber =987678998,
-//             }
 
 
-//            }
-//        }.AsQueryable().BuildMockDbSet().Object);
-//    }
-//    #endregion
-//}
+    }
+
+    #region DatabaseInitilization
+    /// <summary>
+    /// Initializes Mock database with mocked object
+    /// </summary>
+    private void MockPremiumOrderdata()
+    {
+        _mockDbContext.Setup(x => x.Orders).Returns(new List<Order>{new Order()
+        {
+             Id = 1,
+             CustomerId = 2,
+             TotalAmount = 2000,
+             Customer = new Customer
+             {
+                 Name ="Vismaya",
+                 PhoneNumber =987678998,
+             }
+
+
+            }
+        }.AsQueryable().BuildMockDbSet().Object);
+    }
+    #endregion
+}
